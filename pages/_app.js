c7d57567338baf44165bbacaa325c086c929effa
Isiah Fletcher <Isiahfletcher7@gmail.com>
2022-12-13 15:19:18 -0700

    Add app-wide counter

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../faust.config';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { FaustProvider } from '@faustwp/core';
 import 'normalize.css/normalize.css';
@@ -9,8 +9,18 @@ import ThemeStyles from 'components/ThemeStyles/ThemeStyles';
 export default function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    setInterval(() => {
+      setCount((val) => {
+        return val + 1;
+      });
+    }, 1000);
+  }, []);
   return (
     <>
+    <h3>Count: {count}</h3>
       <ThemeStyles />
       <FaustProvider pageProps={pageProps}>
         <Component {...pageProps} key={router.asPath} />
